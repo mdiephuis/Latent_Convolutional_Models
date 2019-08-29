5541119aa8fc461514ab19a8e670e3f58a22dfbb
DIEPHUIS Maurits <diephuis@gauss.unige.ch>
2019-08-29 17:49:17 +0200

    cleanup for my own understanding

--- src/models.py
+++ src/models.py
@@ -8,9 +8,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class ConvBatchLeaky(nn.Conv2d):
+    def __init__(self, lr_slope, *args, **kwargs):
+        super(ConvBatchLeaky, self).__init__(*args, **kwargs)
+        batch_dim = self.weight.data.size(0)
+        self.bn = nn.BatchNorm2d(batch_dim)
+        self.lr = nn.LeakyReLU(lr_slope)
+
+    def forward(self, x):
+        x = super(ConvBatchLeaky, self).forward(x)
+        return self.lr(self.bn(x))
 
 
+class ConvTrBatchLeaky(nn.ConvTranspose2d):
+    def __init__(self, lr_slope, *args, **kwargs):
+        super(ConvTrBatchLeaky, self).__init__(*args, **kwargs)
+        batch_dim = self.weight.data.size(0)
+        self.bn = nn.BatchNorm2d(batch_dim)
+        self.lr = nn.LeakyReLU(lr_slope)
 
+    def forward(self, x):
+        x = super(ConvTrBatchLeaky, self).forward(x)
+        return self.lr(self.bn(x))
 
 
 class EncDecCelebA(nn.Module):
@@ -19,57 +38,46 @@ class EncDecCelebA(nn.Module):
         super(EncDecCelebA, self).__init__()
         self.lr_slope = lr_slope
 
-        self.conv1 = nn.Conv2d(in_channels, 256, 4, 2, 1, 1, bias=False) #16
-        self.conv1_bn = nn.BatchNorm2d(256)
-        self.conv2 = nn.Conv2d(256, 512, 4, 2, 1, 1, bias=False) #8
-        self.conv2_bn = nn.BatchNorm2d(512)
-        self.conv3 = nn.Conv2d(512, 1024, 4, 2, 1, 1, bias=False) #4
-        self.conv3_bn = nn.BatchNorm2d(1024)
+        self.conv1 = ConvBatchLeaky(self.lr_slope, in_channels, 256, 4, 2, 1, 1, bias=False)
+        self.conv2 = ConvBatchLeaky(self.lr_slope, 256, 512, 4, 2, 1, 1, bias=False)  # 8
+        self.conv3 = ConvBatchLeaky(self.lr_slope, 512, 1024, 4, 2, 1, 1, bias=False)  # 4
+        self.conv4 = ConvBatchLeaky(self.lr_slope, 1024, 1024, 3, 1, 2, 2, groups=512, bias=False)  # 4
+        self.conv5 = ConvBatchLeaky(self.lr_slope, 1024, 1024, 3, 1, 2, 2, groups=512, bias=False)  # 4
+        self.conv6 = ConvBatchLeaky(self.lr_slope, 1024, 1024, 3, 1, 2, 2, groups=512, bias=False)  # 4
 
-        self.conv4 = nn.Conv2d(1024, 1024, 3, 1, 2, 2, groups=512, bias=False) #4
-        self.conv4_bn = nn.BatchNorm2d(1024)
-        self.conv5 = nn.Conv2d(1024, 1024, 3, 1, 2, 2, groups=512, bias=False) #4
-        self.conv5_bn = nn.BatchNorm2d(1024)
-        self.conv6 = nn.Conv2d(1024, 1024, 3, 1, 2, 2, groups=512, bias=False) #4
-        self.conv6_bn = nn.BatchNorm2d(1024)
+        self.convT1 = ConvTrBatchLeaky(0.2, 1024 + 1024, 512, 4, 2, 1, bias=bias)  # 8
+        self.convT2 = ConvTrBatchLeaky(0.2, 512 + 512, 512, 4, 2, 1, bias=bias)  # 16
+        self.convT3 = ConvTrBatchLeaky(0.2, 512, 256, 4, 2, 1, bias=bias)  # 32
 
-        self.convT1 = nn.ConvTranspose2d(1024 + 1024, 512, 4, 2, 1, bias=bias)
-        self.convT1_bn = nn.BatchNorm2d(512) #8
-        self.convT2 = nn.ConvTranspose2d(512+512, 512, 4, 2, 1,  bias=bias)
-        self.convT2_bn = nn.BatchNorm2d(512) #16
-        self.convT3 = nn.ConvTranspose2d(512, 256, 4, 2, 1,  bias=bias)
-        self.convT3_bn = nn.BatchNorm2d(256) #32
-        self.convT4 = nn.ConvTranspose2d(256, 128, 4, 2, 1,  bias=bias)
-        self.convT4_bn = nn.BatchNorm2d(128) #64
-        self.convT5 = nn.Conv2d(128, 64, 3, 1, 1,  bias=bias)
-        self.convT5_bn = nn.BatchNorm2d(64) #128
-        self.convT6 = nn.Conv2d(64, 32, 3, 1, 1, 1, bias=bias)
-        self.convT6_bn = nn.BatchNorm2d(32) #128 ##
-        self.convT7 = nn.Conv2d(32, 3, 3, 1, 1, 1, bias=bias) ##
+        self.convT4 = ConvTrBatchLeaky(0.2, 256, 128, 4, 2, 1, bias=bias)  # 64
+        self.convT5 = ConvTrBatchLeaky(0.2, 128, 64, 3, 1, 1, bias=bias)  # 128
+        self.convT6 = ConvTrBatchLeaky(0.2, 64, 32, 3, 1, 1, 1, bias=bias)  # 128
+
+        self.convT7 = nn.Conv2d(32, 3, 3, 1, 1, 1, bias=bias)
         self.upsamp = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-
 
     def forward(self, input):
         #Encoder
-        x1 =  F.leaky_relu(self.conv1_bn(self.conv1(input)), self.lr_slope)
-        x2 =  F.leaky_relu(self.conv2_bn(self.conv2(x1)), self.lr_slope)
-        x3 =  F.leaky_relu(self.conv3_bn(self.conv3(x2)), self.lr_slope)
+        x1 = self.conv1(input)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x2)
 
-        x4 =  F.leaky_relu(self.conv4_bn(self.conv4(x3)), self.lr_slope)
-        x5 =  F.leaky_relu(self.conv5_bn(self.conv5(x4)), self.lr_slope)
-        x6 =  F.leaky_relu(self.conv6_bn(self.conv6(x5)), self.lr_slope)
-
+        x4 = self.conv4(x3)
+        x5 = self.conv5(x4)
+        x6 = self.conv6(x5)
 
         #Decoder
-        x = torch.cat([x6,x3],1)
-        x =  F.leaky_relu(self.convT1_bn(self.convT1(x)), self.lr_slope) #8
-        x = torch.cat([x,x2], 1)
-        x =  F.leaky_relu(self.convT2_bn(self.convT2(x)), self.lr_slope) #16
-        x =  F.leaky_relu(self.convT3_bn(self.convT3(x)), self.lr_slope) #32
-        x =  F.leaky_relu(self.convT4_bn(self.convT4(x)), self.lr_slope) #64
+        x = torch.cat([x6, x3], 1)
+        x = self.convT1(x)  # 8
+        x = torch.cat([x, x2], 1)
+
+        x = self.convT2(x)  # 16
+        x = self.convT3(x)  # 32
+        x = self.convT4(x)  # 64
         x = self.upsamp(x)
-        x =  F.leaky_relu(self.convT5_bn(self.convT5(x)), self.lr_slope) #128
-        x =  F.leaky_relu(self.convT6_bn(self.convT6(x)), self.lr_slope) #128
-        x =  F.sigmoid(self.convT7(x)) #128
+
+        x = self.convT5(x)  # 128
+        x = self.convT6(x)  # 128
+        x = F.sigmoid(self.convT7(x))  # 128
 
         return x
--- src/train_LCM.py
+++ src/train_LCM.py
@@ -2,27 +2,18 @@ import numpy as np
 from tensorboardX import SummaryWriter
 import torch
 import torch.optim
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchvision.utils as vutils
 from data_loader import *
 from models import *
 from custom_losses import *
 from helper_funcs import *
-import itertools
-import time
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
-
-
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 IMG_SIZE = 128
 S_SIZE = 40
-BATCH_SIZE = 10 #was 10
+BATCH_SIZE = 10  # was 10
 LATENT_NET_NAME = 'Latent4LSND'
 RESTRICT = True
 L2_W = 0.0
@@ -37,18 +28,24 @@ MODEL_NAME = 'CelebA150k_LCM'
 LATENT_CHECK_DIR = '../' + MODEL_NAME + '_latentDIR/'
 
 if not os.path.exists(LATENT_CHECK_DIR):
-            os.makedirs(LATENT_CHECK_DIR)
+    os.makedirs(LATENT_CHECK_DIR)
 
 PREV_EPOCH = None
 NUM_EPOCHS = 150000
 SAVE_EVERY = 15000
 
-#The Generator Network
+use_cuda = True
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+device = torch.device("cuda:0" if torch.cuda.is_available() and use_cuda else "cpu")
+
+
+# The Generator Network
 G = EncDecCelebA(in_channels=64)
 G = G.to(device)
 
 #Uniform noise
-s =  get_noise(input_depth=2, method='noise', spatial_size=[S_SIZE, S_SIZE], batch_size=1)
+s = get_noise(input_depth=2, method='noise', spatial_size=[S_SIZE, S_SIZE], batch_size=1)
 
 np.save(MODEL_NAME + '_commoninput', s.data.numpy())
 
@@ -84,19 +81,18 @@ s = s.to(device)
 G_optimizer = optim.SGD(G.parameters(), lr=1.0)
 
 
-
 def trainZG(epoch, data_in, net_in, num_epochs=100):
     """
      Jointly trains the latent networks and the generator network.
      """
     G.train()
     for p in G.parameters():
-        p.requires_grad=True
+        p.requires_grad = True
     BATCH_SIZE = len(net_in)
     nets_params = []
     for i in range(BATCH_SIZE):
         for p in net_in[i].parameters():
-            p.requires_grad=True
+            p.requires_grad = True
         nets_params += list(net_in[i].parameters())
     optim_nets = optim.SGD(nets_params, lr=1.0, weight_decay=L2_W)
     for ep in range(num_epochs):
@@ -117,10 +113,10 @@ def trainZG(epoch, data_in, net_in, num_epochs=100):
         if RESTRICT:
             val = RESTRICT_VAL
             for i in range(BATCH_SIZE):
-                net_in[i].restrict(-1.0*val, val)
+                net_in[i].restrict(-1.0 * val, val)
     optim_nets.zero_grad()
     G_optimizer.zero_grad()
-    if epoch%10 == 0:
+    if epoch % 10 == 0:
         G.eval()
         map_out_lst = []
         for i in range(BATCH_SIZE):
@@ -131,13 +127,12 @@ def trainZG(epoch, data_in, net_in, num_epochs=100):
         writer.add_scalar('Z_loss', loss.data.item(), epoch)
         writer.add_scalar('Z_loss_lap', lap_loss.data.item(), epoch)
         writer.add_scalar('Z_loss_MSE', mse_loss.data.item(), epoch)
-        if epoch%100 == 0:
+        if epoch % 100 == 0:
             writer.add_image('Real_Images', data_in[:5].data.cpu(), epoch)
             writer.add_image('Generated_Images_Z', g_out[:5].data.cpu(), epoch)
-            writer.add_image('latent_Z', map_out[:10,:3,:,:].data.cpu(), epoch)
+            writer.add_image('latent_Z', map_out[:10, :3, :, :].data.cpu(), epoch)
 
     return net_in
-
 
 
 for epoch in range(START_EPOCH, END_EPOCH + 1):
@@ -150,8 +145,8 @@ for epoch in range(START_EPOCH, END_EPOCH + 1):
     #update the latent networks
     data_reader.update_state(latent_nets, latent_net_ids)
     print(fname + " Epoch: ", epoch)
-    if epoch%SAVE_EVERY == 0:
+    if epoch % SAVE_EVERY == 0:
         if epoch > 0:
-            data_reader.save_latent_net(name=MODEL_NAME + "_latentnet_" +str(epoch) + "_", latent_dir=LATENT_CHECK_DIR)
+            data_reader.save_latent_net(name=MODEL_NAME + "_latentnet_" + str(epoch) + "_", latent_dir=LATENT_CHECK_DIR)
             torch.save(G.state_dict(), '../models/CelebA/' + MODEL_NAME + str(epoch))
 writer.close()
